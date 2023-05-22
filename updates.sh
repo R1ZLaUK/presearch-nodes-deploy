d5e3df36dcc08a2077a2a26056154ce940bec716
Jay King <jking710@gmail.com>
2023-05-22 15:27:21 +0100

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set DEBIAN_FRONTEND to noninteractive to automatically accept prompts
+export DEBIAN_FRONTEND=noninteractive
+
 # Update package lists and upgrade installed packages
 sudo apt update && sudo apt upgrade -y
 
