b41175339bd5bad377f2f92e358c54b1033c6329
Jay King <jking710@gmail.com>
2023-05-22 14:55:16 +0100

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install unattended-upgrades
-sudo dpkg-reconfigure unattended-upgrades
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install -y unattended-upgrades
+sudo dpkg-reconfigure -a
+sudo dpkg --purge ubuntu-advantage-tools cloud-init
+sudo apt clean
+sudo apt update
+sudo apt install ubuntu-advantage-tools cloud-init
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo docker run -d --name presearch-auto-updater --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock presearch/auto-updater --cleanup --interval 900 presearch-auto-updater presearch-node
