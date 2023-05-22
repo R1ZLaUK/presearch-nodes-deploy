bc77af31cf7b90fd98ad7c07a0f74195408a6fce
Jay King <jking710@gmail.com>
2023-05-22 15:40:21 +0100

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -3,11 +3,14 @@
 # Set DEBIAN_FRONTEND to noninteractive to automatically accept prompts
 export DEBIAN_FRONTEND=noninteractive
 
-# Update package lists and upgrade installed packages
-sudo apt update && sudo apt upgrade -y
+# Update package lists
+sudo apt update
 
-# Install unattended-upgrades package
-sudo apt-get install -y unattended-upgrades
+# Run unattended upgrades
+sudo unattended-upgrade -d
 
-# Configure unattended-upgrades
-sudo dpkg-reconfigure -plow unattended-upgrades
+# Check if a reboot is required
+if [ -f /var/run/reboot-required ]; then
+  # Schedule a reboot in 1 minute
+  sudo shutdown -r +1 "Server will be restarted in 1 minute."
+fi
