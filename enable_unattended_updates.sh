8bcfafb0347aa2affb0af94cac98d4dbc469c1e2
Jay King <jking710@gmail.com>
2023-05-23 16:48:36 +0100

    Update enable_unattended_updates.sh

--- enable_unattended_updates.sh
+++ enable_unattended_updates.sh
@@ -11,3 +11,9 @@ sudo tee /etc/apt/apt.conf.d/20auto-upgrades <<EOF
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
+
+# Perform initial cleanup of unneeded packages
+sudo apt-get autoremove --purge -y
+
+# Create a cron job to run auto cleanup after 1 day and then revert to 7-day interval
+(crontab -l 2>/dev/null; echo "0 0 * * 1 apt-get autoremove --purge -y") | crontab -
